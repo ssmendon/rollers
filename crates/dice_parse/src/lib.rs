89893a267f18e43b822805dc4a47e096b58f9911
Sohum Mendon <31165513+ssmendon@users.noreply.github.com>
2025-05-31 12:09:47 -0700

    docs(pratt): added some docs, feature flag

--- crates/dice_parse/src/lib.rs
+++ crates/dice_parse/src/lib.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "unstable")]
+#[cfg(feature = "unstable-pratt")]
 pub mod winnow_ext;
 
-#[cfg(not(feature = "unstable"))]
+#[cfg(not(feature = "unstable-pratt"))]
 mod winnow_ext;
--- crates/dice_parse/src/winnow_ext/mod.rs
+++ crates/dice_parse/src/winnow_ext/mod.rs
@@ -1,3 +1,29 @@
+//! Operator precedence parsing for [`winnow`].
+//!
+//! A port of [winnow-rs/winnow#620][winnow-pr] from GitHub user [@39555][ghuser-39555],
+//! because it's currently in draft.
+//!
+//! The best way to learn is by using the test cases and examples in this crate,
+//! since the API for Pratt parsing is verbose.
+//!
+//! The main things to look at are:
+//! * [`precedence::precedence`], the Pratt Parser entrypoint
+//! * [`shunting_yard::precedence`], the Shunting Yard entrypoint
+//!
+//! Slightly less interesting, but also useful:
+//! * [`precedence::Assoc`], the associativity of an operator
+//! * [`precedence::Power`], a type alias for precedence power
+//!
+//! <div class="warning">
+//!
+//! This is not designed for use outside this crate, and it is
+//! gated behind the `unstable-pratt` feature flag.
+//!
+//! </div>
+//!
+//! [winnow-pr]: <https://github.com/winnow-rs/winnow/pull/620>
+//! [ghuser-39555]: <https://github.com/39555>
+
 // Edited from the PR from GitHub user @39555, see winnow-rs/winnow#620.
 // winnow is licensed under MIT terms.
 
